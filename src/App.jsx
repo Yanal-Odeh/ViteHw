ba0361918409a13a2969c6880cc0723e4b01cb2a
Yanal-Odeh <Oudeh.yanal03@gmail.com>
2024-02-16 03:41:15 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let name ='yanal';
+  let age =20;
+
+  return (
+    <>
+      <h2>Hello {name} your age is {age}</h2>
+      
+    </>
+  )
+}
+
+export default App
